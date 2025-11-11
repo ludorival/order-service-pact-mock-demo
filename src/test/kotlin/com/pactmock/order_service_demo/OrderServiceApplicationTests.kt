6a16f6e207c2868b3a7284de32e281cf3e0b2e5b
ludorival <ludorival@gmail.com>
2025-11-11 17:18:53 +0100

    Revert "Step 3 - Implement Pact Verifier (#8)"
    This reverts commit ab7053d72b2cfad1b54d54d6b5105e5acd698681.

---
+++ src/test/kotlin/com/pactmock/order_service_demo/OrderServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.pactmock.order_service_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+class OrderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
